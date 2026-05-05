11adaa515cd781940db50e4b779ae2ac751ccf75
nather2014 <akhilnather@gmail.com>
2026-05-05 19:24:17 +0530

    update

---
+++ src/com/urlshortener/service/UrlService.java
@@ -0,0 +1,19 @@
+class UrlService {
+
+  private final UrlRepository repository;
+  private final CodeGenerator generator;
+
+  UrlService(UrlRepository repository, CodeGenerator generator) {
+    this.repository = repository;
+    this.generator = generator;
+  }
+
+  public String shorten() {
+
+  }
+
+  public String resolve() {
+
+  }
+
+}
