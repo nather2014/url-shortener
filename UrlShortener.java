0beba9327d406ede4c6a1841374d48815d811db3
nather2014 <akhilnather@gmail.com>
2026-05-06 18:08:21 +0530

    update

---
+++ UrlShortener.java
@@ -0,0 +1,22 @@
+import java.util.logging.Logger;
+
+import org.slf4j.LoggerFactory;
+
+class UrlShortener {
+
+  private static final Logger logger = LoggerFactory.getLogger(UrlShortener.class);
+
+  public static void main(String[] args) {
+    String url = "https://www.google.com";
+   
+  }
+
+  public static String shortenUrl(String url) {
+
+    String shortCode = "";
+    
+    logger.info("url is {}",url);
+    return shortCode;
+  }
+
+}
